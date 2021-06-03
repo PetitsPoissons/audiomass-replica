4c3849f6c04b6d139551caad7cddb6989fa2b82b
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-06-03 15:09:11 -0700

    understanding the fireEvent method

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 (function (w, d) {
   'use strict';
-  var _v = '0.9',
+  let _v = '0.9',
     _id = -1;
 
-  function PKAE() {
-    // building q object
-    var q = this; // keeping track of current PKAE (PKAE {})
-    q.el = null; // reference of main html element
-    q.id = ++_id; // auto incremental id
-    q._deps = {}; // dependencies
-    console.log('initializing q object', q);
+  function AudioEditor() {
+    // building thisAE object (thisAE is q in original code)
+    let thisAE = this; // keeping track of current AudioEditor (AudioEditor {})
+    thisAE.el = null; // reference of main html element
+    thisAE.id = ++_id; // auto incremental id
+    thisAE._deps = {}; // dependencies
+    console.log('initializing thisAE object', thisAE);
 
     // understanding window object
     console.log('*************************************************');
-    w.PKAudioList[q.id] = q;
+    w.AudioList[thisAE.id] = thisAE;
     console.log('window w', w);
-    console.log('w.PKAudioList[q.id]', w.PKAudioList[q.id]);
+    console.log('w.AudioList[thisAE.id]', w.AudioList[thisAE.id]);
+
+    // setting up events
+    let events = {};
+    // attaching a fireEvent method to thisAE
+    thisAE.fireEvent = function (eventName, value, valueBis) {
+      // eventName is an array of functions
+      let eventArr = events[eventName];
+      if (!eventArr) return false;
+      // trigger the functions listed within the eventName array
+      let maxIndex = eventArr.length;
+      while (maxIndex-- > 0) {
+        eventArr[maxIndex] && eventArr[maxIndex](value, valueBis);
+      }
+    };
   }
-  !w.PKAudioList && (w.PKAudioList = []);
-  w.PKAudioEditor = new PKAE();
-  PKAudioList.push(w.PKAudioEditor);
+
+  !w.AudioList && (w.AudioList = []);
+  w.AudioEditor = new AudioEditor();
+  AudioList.push(w.AudioEditor); // duplicating the current AudioEditor
 })(window, document);
